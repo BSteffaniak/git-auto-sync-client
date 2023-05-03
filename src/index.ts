d1f0f2e1a09c3184cb700b3b0e06e7dbc39605d8
Braden Steffaniak <BradenSteffaniak@gmail.com>
2023-05-02 22:44:06 -0400

    Handle auto reconnecting

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,80 @@
 import WebSocket from "ws";
+import winston from "winston";
 
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
-function heartbeat(this: any) {
-  clearTimeout(this.pingTimeout);
+const logger = winston.createLogger({
+  level: process.env.NODE_ENV === "production" ? "error" : "debug",
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    }),
+  ],
+});
 
-  // Use `WebSocket#terminate()`, which immediately destroys the connection,
-  // instead of `WebSocket#close()`, which waits for the close timer.
-  // Delay should be equal to the interval at which your server
-  // sends out pings plus a conservative assumption of the latency.
-  this.pingTimeout = setTimeout(() => {
-    this.terminate();
-    console.log("Terminated");
-  }, 30000 + 1000);
+const MAX_CONNECTION_RETRY_COUNT = parseInt(
+  process.env.MAX_CONNECTION_RETRY_COUNT!
+);
+const CONNECTION_RETRY_DEBOUNCE = parseInt(
+  process.env.CONNECTION_RETRY_DEBOUNCE!
+);
+
+async function newClient() {
+  return new Promise((resolve) => {
+    const client = new WebSocket(process.env.WEBSOCKET_SERVER_URL!);
+
+    let opened = false;
+
+    client.on("error", function(...args) {
+      console.error.apply(this, args);
+
+      if (!opened) resolve(false);
+    });
+
+    client.on("open", function open(this: any) {
+      opened = true;
+      logger.info("Connection has successfully been opened");
+      client.send(JSON.stringify({ action: "sendmessage", data: "test" }));
+
+      resolve(true);
+    });
+
+    client.on("close", async function clear(this: any) {
+      logger.info("Closed");
+
+      client.terminate();
+
+      await attemptConnection();
+    });
+
+    client.on("message", function message(data) {
+      logger.info(`received: ${data}`);
+    });
+  });
 }
 
-const client = new WebSocket(process.env.WEBSOCKET_SERVER_URL!);
+async function attemptConnection() {
+  for (
+    let i = 0;
+    MAX_CONNECTION_RETRY_COUNT === -1 || i < MAX_CONNECTION_RETRY_COUNT;
+    i++
+  ) {
+    logger.info(`Attempting connection. Attempt ${i + 1}`);
+    if (await newClient()) {
+      logger.info("Successfully connected client");
+      return;
+    }
 
-client.on("error", console.error);
+    await new Promise((resolve) => {
+      logger.info(`Failed to connect. Waiting ${CONNECTION_RETRY_DEBOUNCE}ms`);
+      setTimeout(resolve, CONNECTION_RETRY_DEBOUNCE);
+    });
+  }
 
-client.on("open", function () {
-  heartbeat.apply(this);
-});
+  console.error("Failed to establish connection to websocket server");
+  process.exit(1);
+}
 
-client.on("ping", function () {
-  heartbeat.apply(this);
-});
-
-client.on("close", function clear(this: any) {
-  clearTimeout(this.pingTimeout);
-});
-
-client.on('message', function message(data) {
-  console.log('received: %s', data);
-});
+await attemptConnection();
