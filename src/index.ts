68311f9e438a6a9b6d6540509d4ed2b2d3fcdb7a
Braden Steffaniak <BradenSteffaniak@gmail.com>
2023-05-06 18:26:41 -0400

    Update attemptConnection to return a WebSocket connection on success
    Start an express server and listen to /send POST

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
+import express, { Express } from "express";
 import WebSocket from "ws";
 import winston from "winston";
-
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const logger = winston.createLogger({
   level: process.env.NODE_ENV === "production" ? "error" : "debug",
-  format: winston.format.combine(winston.format.timestamp(), winston.format.simple()),
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.simple()
+  ),
   transports: [
     new winston.transports.Console({
       format: winston.format.simple(),
@@ -22,17 +25,17 @@ const CONNECTION_RETRY_DEBOUNCE = parseInt(
   process.env.CONNECTION_RETRY_DEBOUNCE!
 );
 
-async function newClient() {
-  return new Promise((resolve) => {
+async function newClient(): Promise<WebSocket> {
+  return new Promise((resolve, reject) => {
     let pingInterval: NodeJS.Timeout | undefined;
     const client = new WebSocket(process.env.WEBSOCKET_SERVER_URL!);
 
     let opened = false;
 
-    client.on("error", function(...args) {
+    client.on("error", function (...args) {
       logger.error(JSON.stringify(args));
 
-      if (!opened) resolve(false);
+      if (!opened) reject();
     });
 
     client.on("open", function open(this: any) {
@@ -41,12 +44,12 @@ async function newClient() {
 
       pingInterval = setInterval(() => {
         if (!opened) return clearInterval(pingInterval);
-        
+
         logger.info("Sending ping");
         client.ping();
       }, 9 * 60 * 1000);
 
-      resolve(true);
+      resolve(client);
     });
 
     client.on("close", async function clear(this: any) {
@@ -65,27 +68,61 @@ async function newClient() {
   });
 }
 
-async function attemptConnection() {
-  for (
-    let i = 0;
-    MAX_CONNECTION_RETRY_COUNT === -1 || i < MAX_CONNECTION_RETRY_COUNT;
-    i++
-  ) {
-    logger.info(`Attempting connection. Attempt ${i + 1}`);
+async function attemptConnection(): Promise<WebSocket> {
+  let attemptNumber = 0;
 
-    if (await newClient()) {
+  while (true) {
+    logger.info(`Attempting connection. Attempt ${attemptNumber + 1}`);
+
+    try {
+      const ws = await newClient();
+
       logger.info("Successfully connected client");
-      return;
-    }
 
-    await new Promise((resolve) => {
-      logger.info(`Failed to connect. Waiting ${CONNECTION_RETRY_DEBOUNCE}ms`);
-      setTimeout(resolve, CONNECTION_RETRY_DEBOUNCE);
-    });
+      return ws;
+    } catch (e: unknown) {
+      if (
+        MAX_CONNECTION_RETRY_COUNT !== -1 &&
+        attemptNumber++ === MAX_CONNECTION_RETRY_COUNT
+      ) {
+        break;
+      }
+
+      await new Promise((resolve) => {
+        logger.info(
+          `Failed to connect. Waiting ${CONNECTION_RETRY_DEBOUNCE}ms`
+        );
+        setTimeout(resolve, CONNECTION_RETRY_DEBOUNCE);
+      });
+    }
   }
 
   console.error("Failed to establish connection to websocket server");
   process.exit(1);
 }
 
-await attemptConnection();
+async function startServer(): Promise<Express> {
+  return new Promise((resolve) => {
+    const app = express();
+    const port = 3000;
+
+    app.use(express.json());
+
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+      resolve(app);
+    });
+  });
+}
+
+console.time("startup");
+const [ws, server] = await Promise.all([attemptConnection(), startServer()]);
+console.timeEnd("startup");
+
+server.post("/send", (req, res) => {
+  console.time("POST /send");
+  const { data } = req.body;
+  ws.send(JSON.stringify({ action: "sendmessage", data }));
+  res.send({ success: true });
+  console.timeEnd("POST /send");
+});
